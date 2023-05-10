0b2b2e33acaa859d8199247d73f91c65ed57ebc2
Souvik Biswas <sbis1999@gmail.com>
2023-05-10 17:29:11 +0530

    Minor cloud function update

--- functions/index.js
+++ functions/index.js
@@ -8,18 +8,23 @@ const {
 dotenv.config();
 
 exports.generateToken = functions.https.onCall(async (data, context) => {
-  // Rtc Examples
   const appId = process.env.APP_ID;
   const appCertificate = process.env.APP_CERTIFICATE;
   const channelName = data.channelName;
-  const uid = 0;
+  const uid = data.uid || 0;
   const role = RtcRole.PUBLISHER;
 
   const expirationTimeInSeconds = data.expiryTime;
   const currentTimestamp = Math.floor(Date.now() / 1000);
   const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
 
-  // Build token with uid
+  if (channelName === undefined || channelName === null) {
+    throw new functions.https.HttpsError(
+        "aborted",
+        "Channel name is required",
+    );
+  }
+
   try {
     const token = RtcTokenBuilder.buildTokenWithUid(
         appId,
@@ -31,10 +36,6 @@ exports.generateToken = functions.https.onCall(async (data, context) => {
     );
     return token;
   } catch (err) {
-    console.error(
-        `Unable to generate token ${context.auth.uid}. 
-        Error ${err}`,
-    );
     throw new functions.https.HttpsError(
         "aborted",
         "Could not generate token",
